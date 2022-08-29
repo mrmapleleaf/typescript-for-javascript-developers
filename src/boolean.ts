f69e4b46dbb50cdac9d52dd714b94c464d89dce3
mrmapleleaf <kaple.de.96@gmail.com>
2022-08-30 00:23:43 +0900

    fix ts-node-js issue and modify package.json

---
+++ src/boolean.ts
@@ -0,0 +1,13 @@
+export {};
+
+let name = 'TypeScript';
+
+//javascriptの書き方
+// let isFinished = true;
+// console.log({isFinished});
+
+//typescriptの書き方
+let isFinished: boolean = true;
+isFinished = false;
+//isFinished = 1;
+console.log({isFinished});
